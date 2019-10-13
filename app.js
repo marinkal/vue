603f63177cf58ccf0d5c22d49817f2965bb0b8f2
marina_lozhkina <m.a.lozhkina@tmn2.etagi.com>
2019-10-13 20:15:38 +0500

    lesson4

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const car = (name, model, year, owner, phone, image) => ({name, model, year, own
 let cars = 
     [
         car("Ford", "Focus", "2015", "Max", "+7 912 078 46 81", "images/focus.png"),
-        car("Ford", "Mondeo", "2018", "irina", "+7 932 344 32 12", "images/mondeo.jpg"),
-        car("Kia", "Ria", "2016", "Valentina", "+7 902 343 17 18", "images/kia.jpg"),
+        car("Ford", "Mondeo", "2018", "irina", "+7 932 344 32 12", "images/mondeo.png"),
+        car("Kia", "Ria", "2016", "Valentina", "+7 902 343 17 18", "images/kia.png"),
         
     ]
 
 new Vue({
     el: "#app",
     data: {
-        cars: cars
+        cars: cars,
+        selectedCarIndex: 0,
+        car: cars[0],
+        phoneVisibility: false
+    },
+    methods: {
+        selectedCar: function(index) {
+            this.car = cars[index];
+            this.selectedCarIndex = index;
+        }
     }
 });
